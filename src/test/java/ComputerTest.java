0293660adf3a9f33ffcc81e6e02ebd08bbce40e8
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2024-09-30 17:18:43 -0500

    Update ComputerTest.java

--- src/test/java/ComputerTest.java
+++ src/test/java/ComputerTest.java
@@ -14,14 +14,14 @@ public class ComputerTest {
     public void testCard(){
         Machine machine = new Machine();
 
-        // addCard(Card, slot)
+        // addCard(Card(id), slot)
         machine.addCard(new DeviceCard(1), 0);
         machine.addCard(new DeviceCard(2), 1);
         machine.addCard(new ExtensionBoard(3), 2);
 
         assertEquals(2, machine.getNumberOfDeviceCards());
 
-        // addCard(slotWithExtensionCard, Card, slotWithinExtensionCard
+        // addCard(slotWithExtensionCard, Card(id), slotWithinExtensionCard
         machine.addCard(2,new DeviceCard(4), 0);
 
         assertEquals(3, machine.getNumberOfDeviceCards());
